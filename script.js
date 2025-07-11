abf1d955d22065fe434c680782b47413dbd40be6
Abhishek Sachan <66594749+Sachan-dev@users.noreply.github.com>
2025-07-11 20:34:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function selectApp(appName) {
-  const message = `This app activated: ${appName}`;
-  Telegram.WebApp.sendData(message);  // Send to bot
-  Telegram.WebApp.close();            // Close web app
+  Telegram.WebApp.sendData(`This app activated: ${appName}`);
+  Telegram.WebApp.close();
 }
